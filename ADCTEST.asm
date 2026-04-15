7c4d0c247d4c564426a39637d1e63a5e282c8d16
adulam <arjunkumardulam@gmail.com>
2026-04-15 16:37:09 -0400

    added proper comment syntax to ADCTEST.asm

--- ADCTEST.asm
+++ ADCTEST.asm
@@ -6,11 +6,11 @@ HEX_HI:     EQU &H005
 ORG 0
 
 Loop:
-    -- Upper two digits show the status register, including busy on bit 1.
+    ;  Upper two digits show the status register, including busy on bit 1.
     IN ADC_STATUS
     OUT HEX_HI
 
-    -- Lower four digits show the most recent 12-bit ADC sample.
+    ; Lower four digits show the most recent 12-bit ADC sample.
     IN ADC_DATA
     OUT HEX_LO
 

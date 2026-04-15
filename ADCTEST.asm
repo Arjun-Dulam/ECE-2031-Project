f89b4f66d9f9a9238c41600362b0ae96ab2b50c8
adulam <arjunkumardulam@gmail.com>
2026-04-15 16:21:59 -0400

    finished SCOMP from pdf

--- ADCTEST.asm
+++ ADCTEST.asm
@@ -1,12 +1,17 @@
+ADC_DATA:   EQU &H0C0
+ADC_STATUS: EQU &H0C1
+HEX_LO:     EQU &H004
+HEX_HI:     EQU &H005
+
 ORG 0
+
 Loop:
-IN &H0C1
-OUT &H005
+    -- Upper two digits show the status register, including busy on bit 1.
+    IN ADC_STATUS
+    OUT HEX_HI
 
-IN &H0C0
-OUT &H004
+    -- Lower four digits show the most recent 12-bit ADC sample.
+    IN ADC_DATA
+    OUT HEX_LO
 
-JUMP Loop
-
-
-ADC: EQU &H0C0
+    JUMP Loop
